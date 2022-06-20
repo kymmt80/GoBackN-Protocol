be1491aa9ab7ee58ba000828125c2fc1bd015db9
kymmt80 <kimiya80@gmail.com>
2022-06-20 19:15:39 +0430

    feat: add buffer to router

--- router/include/router.hpp
+++ router/include/router.hpp
@@ -2,9 +2,10 @@
 #define __ROUTER_HPP__
 
 #include <vector>
+#include <queue>
 
 #include "../../utils/include/socket.hpp"
-
+#include "../../utils/defs.hpp"
 
 class Router {
     public:
@@ -16,6 +17,9 @@ class Router {
             int port_from_receiver
         );
         void run();
+        void add_to_buffer(frame message);
+        void pop_buffer();
+        bool buffer_timeout();
     private:
         char* ip;
         int sender_send_fd;
@@ -23,6 +27,8 @@ class Router {
         int receiver_send_fd;
         int receiver_receive_fd;
         std::vector<Socket*>sockets;
+        std::queue<frame> buffer; 
+        clock_t last_send;
 };
 
 #endif
